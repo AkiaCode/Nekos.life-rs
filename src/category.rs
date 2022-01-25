21a94a3f617108f9254988edc7e9984b3eb96cf4
Abiria <abiria.dev@gmail.com>
2022-01-25 10:40:25 +0900

    test(category): added tests to Category
    - added tests to ensure that all derived macros of `strum` are wrok
    correctly.

--- src/category.rs
+++ src/category.rs
@@ -121,3 +121,36 @@ impl Category {
         self.into()
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use {super::*, strum::IntoEnumIterator};
+
+    #[test]
+    fn can_be_displayed() {
+        // ensures that the `strum::Display` was derived correctly.
+        Category::iter().for_each(|variant| {
+            format!("{variant}");
+        });
+    }
+
+    #[test]
+    fn can_be_stringified() {
+        // ensures that the `strum::IntoStaticStr` was derived correctly.
+        Category::iter().for_each(|variant| {
+            let _ = Into::<&'static str>::into(&variant);
+        })
+    }
+
+    #[test]
+    fn two_methods_result_in_same_string() {
+        // ensures that
+        // the `<Category as std::string::ToString>::to_string()` method
+        // and the `<Category as Into<&'static str>>::into()` method
+        // result in the same string.
+        assert!(Category::iter()
+            .map(|variant| variant.to_string())
+            .eq(Category::iter()
+                .map(Into::<&'static str>::into)));
+    }
+}
