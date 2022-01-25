2988db6f2c7cdcd50aa7b69e80dfe62e35ae1249
Abiria <abiria.dev@gmail.com>
2022-01-25 11:51:11 +0900

    docs(implementation): added doc comments and examples

--- src/implementation.rs
+++ src/implementation.rs
@@ -1,17 +1,60 @@
 use crate::{r#static::BASEURL, Category, NekosLifeError};
 
-// use super::*;
-
-/// Gets the url of an image / gif from the api, from the given category,
-/// and using the given client.
+/// Gets the image url with the given client.
+///
+/// # Note
+///
+/// ## Context
+///
+/// this function returns [Future](std::future::Future) which must be awaited.
+/// so you have to run this in an async context.
+/// if you don't care about performance or only need blocking API,
+/// check the [`blocking_get`](create::blocking_get) function out, which is blocking version of this.
+///
+/// ## Reusability
+///
+/// this function takes a [client](reqwest::Client) as an argument,
+/// so if you have to make countless number of requests.\
+/// it is good idea to reuse the same client for each request,
+/// by using this function instead of [`blocking_get`].
+///
+/// most cases, however, will not need to call this directly.
+/// if you find some simplest way, consider using [`blocking_get`](blocking_get) instead.
+///
+/// # Returns
+///
+/// On success, the [`Ok(String)`](String) which contains the `URL` of the image or GIF
+/// found by using the given endpoint from the `API` returns.\
+/// Otherwise, it will return the [`Err(NekosLifeError)`](NekosLifeError) if any error is encountered.
+///
 /// # Examples
+///
 /// ```rust,no_run
-/// # #[tokio::main]
-/// # async fn main() -> Result<(), Box<dyn std::error::Error>> {
-///     let client = reqwest::Client::new();
-///     let url: String = nekoslife::get_with_client(&client, nekoslife::Category::Waifu).await?;
-/// #   Ok(())
-/// # }
+/// # tokio::runtime::Runtime::new().unwrap().block_on(async {
+/// // declare the client we use
+/// let client = reqwest::Client::new();
+///
+/// // and repeat the request 5 times.
+/// for category in <
+///     nekoslife::Category as strum::IntoEnumIterator
+/// >::iter()
+///     .take(5)
+/// {
+///     // get the url with blocking context. (no 'await')
+///     // note that we pass the reference of the client
+///     // as the first argument at here.
+///     let url = nekoslife::blocking_get_with_client(
+///         &client,
+///         category,
+///     ).await?;
+///     // then print the each result.
+///     println!("{url}");
+/// }
+/// # Ok::<(), nekoslife::NekosLifeError>(())
+/// # });
+/// ```
+///
+/// [get_with_client]: crate::get_with_client
 pub async fn get_with_client(
     client: &reqwest::Client,
     category: impl Into<Category>,
@@ -37,15 +80,44 @@ pub async fn get_with_client(
     Ok(resp.url)
 }
 
-/// Gets the url of an image / gif from the api, from the given
-/// category, and using the default client.
+/// Gets the image url
+/// 
+/// # Note
+///
+/// ## Context
+///
+/// this function returns [Future](std::future::Future) which must be awaited.
+/// so you have to run this in an async context.
+/// if you don't care about performance or only need blocking API,
+/// check the [`blocking_get`](create::blocking_get) function out, which is blocking version of this.
+///
+/// ## Reusability
+///
+/// also this function will make new [client](reqwest::Client)
+/// struct with default settings every time it is called.\
+/// if you have to reuse the client or set your client carefully,
+/// consider using the [`get_with_client`] function instead.
+///
+/// # Returns
+///
+/// On success, the [`Ok(String)`](String) which contains the `URL` of the image or GIF
+/// found by using the given endpoint from the `API` returns.\
+/// Otherwise, it will return the [`Err(NekosLifeError)`](NekosLifeError) if any error is encountered.
+///
 /// # Examples
+///
 /// ```rust,no_run
-/// # #[tokio::main]
-/// # async fn main() -> Result<(), Box<dyn std::error::Error>> {
-///     let url: String = nekoslife::get(nekoslife::Category::Waifu).await?;
-/// #   Ok(())
-/// # }
+/// # tokio::runtime::Runtime::new().unwrap().block_on(async {
+/// // get the url from 'Waifu' category
+/// let url = nekoslife::get(nekoslife::Category::Waifu).await?;
+/// 
+/// // then print the url.
+/// println!("{url}");
+/// # Ok::<(), nekoslife::NekosLifeError>(())
+/// });
+/// ```
+///
+/// [get_with_client]: crate::get_with_client
 pub async fn get(
     category: impl Into<Category>,
 ) -> Result<String, NekosLifeError> {
