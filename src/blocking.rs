191bb681b006bc2b503a13f67457d1f0650d1da1
Abiria <abiria.dev@gmail.com>
2022-01-25 09:27:23 +0900

    docs(blocking): added more doc comments and examples

--- src/blocking.rs
+++ src/blocking.rs
@@ -3,15 +3,58 @@ use crate::{
     implementation::get_with_client,
 };
 
-/// Gets the url of an image / gif from the api, from the given category,
-/// and using the given client.
+/// Gets the image url in blocking context with the given client.
+///
+/// # Note
+///
+/// ## Context
+///
+/// this is the `blocking` version of the [`get_with_client`][get_with_client] function.\
+/// if you want to use it in async contexts or performance is matters,
+/// consider using the [`get_with_client`][get_with_client] function instead.
+///
+/// ## Reusability
+///
+/// this function takes a [client](reqwest::Client) as an argument,
+/// so if you have to make countless number of requests.\
+/// it is good idea to reuse the same client for each request,
+/// by using this function instead of [`blocking_get`].
+///
+/// most cases, however, will not need to call this directly.
+/// if you find some simplest way, consider using [`blocking_get`](blocking_get) instead.
+///
+/// # Returns
+///
+/// On success, the [`Ok(String)`](String) which contains the `URL` of the image or GIF
+/// found by using the given endpoint from the `API` returns.\
+/// Otherwise, it will return the [`Err(NekosLifeError)`](NekosLifeError) if any error is encountered.
+///
 /// # Examples
+///
 /// ```rust,no_run
-/// # fn main() -> Result<(), Box<dyn std::error::Error>> {
-///     let client = reqwest::blocking::Client::new();
-///     let url: String = nekoslife::get_with_client(&client, nekoslife::SfwCategory::Waifu)?;
-/// #   Ok(())
-/// # }
+/// // declare the client we use
+/// let client = reqwest::Client::new();
+///
+/// // and repeat the request 5 times.
+/// for category in <
+///     nekoslife::Category as strum::IntoEnumIterator
+/// >::iter()
+///     .take(5)
+/// {
+///     // get the url with blocking context. (no 'await')
+///     // note that we pass the reference of the client
+///     // as the first argument at here.
+///     let url = nekoslife::blocking_get_with_client(
+///         &client,
+///         category,
+///     )?;
+///     // then print the each result.
+///     println!("{url}");
+/// }
+/// # Ok::<(), nekoslife::NekosLifeError>(())
+/// ```
+///
+/// [get_with_client]: crate::get_with_client
 pub fn blocking_get_with_client(
     client: &reqwest::Client,
     category: impl Into<Category>,
@@ -23,14 +66,38 @@ pub fn blocking_get_with_client(
         .block_on(get_with_client(&client, category.into()))
 }
 
-/// Gets the url of an image / gif from the api, from the given category,
-/// and using the default client.
+/// Gets the image url in blocking context.
+///
+/// # Note
+///
+/// ## Context
+///
+/// this is the `blocking` version of the [`get`][get] function.\
+/// if you want to use it in async contexts or performance is matters,
+/// consider using the [`get`][get] function instead.
+///
+/// ## Reusability
+///
+/// also this function will make new [client](reqwest::Client)
+/// struct with default settings every time it is called.\
+/// if you have to reuse the client or set your client carefully,
+/// consider using the [`blocking_get_with_client`] function instead.
+///
+///
+/// # Returns
+///
+/// On success, the [`Ok(String)`](String) which contains the `URL` of the image or GIF
+/// found by using the given endpoint from the `API` returns.\
+/// Otherwise, it will return the [`Err(NekosLifeError)`](NekosLifeError) if any error is encountered.
+///
 /// # Examples
+///
 /// ```rust,no_run
-/// # fn main() -> Result<(), Box<dyn std::error::Error>> {
-///     let url: String = nekoslife::get(nekoslife::SfwCategory::Waifu)?;
-/// #   Ok(())
-/// # }
+/// let url: String = nekoslife::blocking_get(nekoslife::Category::Waifu)?;
+/// # Ok::<(), nekoslife::NekosLifeError>(())
+/// ```
+///
+/// [get]: crate::get
 pub fn blocking_get(
     category: impl Into<Category>,
 ) -> Result<String, NekosLifeError> {
