7e04ad0cc8da7a18492fcfee0ef1ebff2bea239a
Abiria <abiria.dev@gmail.com>
2022-01-27 17:07:57 +0900

    feat(text): added 'owoify/' endpoint
    - removed `Text` enum and made indiviual tuple structs and models.
    - added `OwOify` endpoint.
    	- added tests for `OwOify`.
    	- added condition to check whether the length of inner text of `OwOify` is valid.

--- src/implementation/text.rs
+++ src/implementation/text.rs
@@ -1,29 +1,60 @@
 use {crate::r#static::BASEURL, serde::Deserialize};
 
-#[derive(Debug, strum::IntoStaticStr, PartialEq, Eq)]
-#[strum(serialize_all = "snake_case")]
-pub enum Text {
-    Cat,
-}
+struct Cat;
 
 #[derive(Deserialize)]
-pub struct Cat {
+pub struct CatModel {
     cat: crate::UrlString,
 }
 
-impl super::types::IntoUrl for Text {
+impl super::types::IntoUrl for Cat {
     type Response = crate::types::UrlString;
 
     type Fut = into_url_fut! {};
 
     fn into_url(self) -> crate::types::Result<url::Url> {
-        Ok(BASEURL
-            .join(Into::<&'static str>::into(&self))?)
+        Ok(BASEURL.join("cat")?)
     }
 
     parse_json! {
-        Cat,
+        CatModel,
         cat,
+    }
+}
+
+struct OwOify<'a>(pub &'a str);
+
+#[derive(Deserialize)]
+pub struct OwOifyModel {
+    owo: crate::UrlString,
+}
+
+impl<'a> super::types::IntoUrl for OwOify<'a> {
+    type Response = crate::types::UrlString;
+
+    type Fut = into_url_fut! {};
+
+    fn into_url(self) -> crate::types::Result<url::Url> {
+        Ok({
+            let mut url = BASEURL.join("owoify")?;
+
+            url.query_pairs_mut()
+                .append_pair(
+                    "text", 
+                    if matches! {
+                        self.0.len(),
+                        1..=200
+                    } { self.0 } else {
+                        panic!("OwOify text must be between 1 and 200 characters")
+                    }
+                );
+            url
+        })
+    }
+
+    parse_json! {
+        OwOifyModel,
+        owo
     }
 }
 
--- src/implementation/text/tests.rs
+++ src/implementation/text/tests.rs
@@ -1,16 +1,67 @@
 use {
     super::*,
-    crate::{get_with_client, NekosLifeError},
+    crate::{get, get_with_client, NekosLifeError},
+    pretty_assertions::assert_eq,
     // lazy_regex::{lazy_regex, Lazy, Regex},
     reqwest::Client,
 };
 
 // static RESULT_CAT: Lazy<Regex> = lazy_regex!("qwer");
 
+const EXACTLY_200_CHAR: &str =
+    const_format::str_repeat! {"a", 200};
+
 #[tokio::test]
 async fn get_with_client_and_cat_test(
 ) -> Result<(), NekosLifeError> {
-    dbg!(get_with_client(&Client::new(), Text::Cat).await?);
+    dbg!(get_with_client(&Client::new(), Cat).await?);
 
     Ok(())
+}
+
+#[tokio::test]
+async fn get_with_client_and_owoify_test(
+) -> Result<(), NekosLifeError> {
+    Ok(assert_eq!(
+        get_with_client(
+            &Client::new(),
+            OwOify("Abiria is cute and kawaii :)")
+        )
+        .await?,
+        "Abiwia is cute anyd kawaii :)"
+    ))
+}
+
+#[tokio::test]
+async fn owoify_with_exactly_200_chars(
+) -> Result<(), NekosLifeError> {
+    assert_eq!(EXACTLY_200_CHAR.len(), 200);
+
+    assert_eq!(
+        get(OwOify(&EXACTLY_200_CHAR)).await?,
+        EXACTLY_200_CHAR
+    );
+
+    Ok(())
+}
+
+#[tokio::test]
+#[should_panic]
+async fn owoify_with_more_than_200_chars() {
+    assert!(get(OwOify(const_format::concatcp! {
+       EXACTLY_200_CHAR,
+       "a"
+    }))
+    .await
+    .is_err());
+}
+
+#[tokio::test]
+#[should_panic]
+async fn owoify_with_0_chars() {
+    assert!(
+        get_with_client(&Client::new(), OwOify(""))
+            .await
+            .is_err()
+    );
 }
