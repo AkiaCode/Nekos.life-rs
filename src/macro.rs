ec227cfa7a5312d5f79d064f313380e215026892
Abiria <abiria.dev@gmail.com>
2022-01-27 23:05:19 +0900

    refactor(text): organized all code into macro

--- src/macro.rs
+++ src/macro.rs
@@ -78,7 +78,7 @@ macro_rules! make_text_endpoints {
         }
     };
     (
-        ||= $i: tt >=>
+        ||= $ i: tt >=>
         $ cb: ident |>
         $ (
             $ arg: tt
@@ -92,168 +92,189 @@ macro_rules! make_text_endpoints {
         }
     };
     (
-        |-> $endpoint :ident
-        ->> $field :ident
+        |-> $ endpoint :ident
+        ->> $ field :ident
     ) => {
         paste::paste! {
-            #[derive(Deserialize)]
-            pub struct [<
-                $endpoint
+            #[derive(
+                serde::Deserialize
+            )] pub struct [<
+                $ endpoint
                 Model
             >] {
-                $field: crate::UrlString,
+                $ field: $ crate::UrlString,
             }
         }
     };
     (
         $ (
-            #[
-                $ attr :meta
-            ]
-        ) *
-        $ endpoint :ident
-        $ (
-            @ $l :lifetime
-        ) ?
-        $ (
-           |>
-                $ (
-                    $ inner_type: ty
-                ) ?
-                :=
-                $ (
-                    $ inner :ident
-                ) ?
-            <|
-        ) ?
-        $ (
-            ~~> $ ep_alias :ident
-        ) ?
-        $ (
-            !# $range :expr
-        ) ?
+            $ (
+                #[
+                    $ attr :meta
+                ]
+            ) *
+            $ endpoint :ident
+            $ (
+                @ $ l :lifetime
+            ) ?
+            $ (
+                |>
+                    $ (
+                        $ inner_type: ty
+                    ) ?
+                    :=
+                    $ (
+                        $ inner :ident
+                    ) ?
+                <|
+            ) ?
+            $ (
+                ~~> $ ep_alias :ident
+            ) ?
+            $ (
+                !# $range :expr
+            ) ?
+        ) => +
     ) => {
         $ (
-            #[
-                $ attr
-            ]
-        ) *
-        pub struct $endpoint $(<$l>)? $ (
-            (
-                pub & $ l
-                $ (
-                    $ inner_type
-                ) ?
-            )
-        ) ? ;
-
-        paste::paste! {
-            make_text_endpoints! {
-                $ (
+            #[derive(
+                Debug,
+                PartialEq,
+                Eq,
+                Clone,
+                Copy,
+            )] $ (
+                #[
+                    $ attr
+                ]
+            ) *
+            pub struct $ endpoint $ (
+                <
+                    $ l
+                >
+            ) ? $ (
+                (
+                    pub & $ l
                     $ (
-                        $inner
+                        $ inner_type
                     ) ?
-                ) ?
-                    ||=
-                [<
-                    $endpoint :lower
-                >]
-                    >=>
-                make_text_endpoints |>
-                    |-> $endpoint
-                    ->>
-            }
-        }
-
-        impl $ (
-            <
-                $l
-            >
-        ) ? $crate::implementation::types::IntoUrl
-        for $endpoint
-        $ (
-            <
-                $ l
-            >
-        ) ? {
-            type Response = $crate::types::UrlString;
-
-            type Fut = into_url_fut! {};
-
-            fn into_url(
-                self,
-            ) -> $crate::types::Result<
-                url::Url
-            > {
-                paste::paste! {
-                    Ok({
-                        #[allow(
-                            unused_mut
-                        )] let mut url: url::Url =
-                            $crate::r#static::BASEURL
-                                .join(
-                                    make_text_endpoints! {
-                                        $ (
-                                            $ep_alias
-                                        ) ?
-                                            ||=
-                                        [<
-                                            $endpoint :lower
-                                        >]
-                                            >=>
-                                        stringify |>
-                                    }
-                                ) ? ;
-
-                        $ (
-                            $ (
-                                url
-                                    .query_pairs_mut()
-                                    .append_pair(
-                                        "text",
-                                        if matches! {
-                                            (self.0 as &$ l $ inner_type)
-                                                .len(),
-                                            $range
-                                        } {
-                                            self.0
-                                        } else {
-                                            Err(NekosLifeError::OutOfRangeError {
-                                                endpoint_name: stringify! {
-                                                    $endpoint
-                                                }.to_owned(),
-                                                range: $range,
-                                            })?
-                                        }
-                                    );
-                            ) ?
-                        ) ?
-
-                        url
-                    })
-                }
-            }
+                )
+            ) ? ;
 
             paste::paste! {
                 make_text_endpoints! {
                     $ (
                         $ (
-                            $inner
+                            $ inner
                         ) ?
                     ) ?
                         ||=
                     [<
-                        $endpoint :lower
+                        $ endpoint :lower
                     >]
                         >=>
-                    parse_json |>
-                        [<
-                            $endpoint
-                            Model
-                        >]
-                        ,
+                    make_text_endpoints |>
+                        |-> $ endpoint
+                        ->>
                 }
             }
-        }
+
+            impl $ (
+                <
+                    $ l
+                >
+            ) ? $ crate
+                ::implementation
+                ::types
+                ::IntoUrl
+            for $ endpoint $ (
+                <
+                    $ l
+                >
+            ) ? {
+                type Response = $ crate::types::UrlString;
+
+                type Fut = into_url_fut! {};
+
+                fn into_url(
+                    self,
+                ) -> $ crate::types::Result<
+                    url::Url
+                > {
+                    paste::paste! {
+                        Ok({
+                            #[allow(
+                                unused_mut
+                            )] let mut url: url::Url =
+                                $ crate
+                                    ::r#static
+                                    ::BASEURL
+                                        .join(
+                                            make_text_endpoints! {
+                                                $ (
+                                                    $ ep_alias
+                                                ) ?
+                                                    ||=
+                                                [<
+                                                    $ endpoint :lower
+                                                >]
+                                                    >=>
+                                                stringify |>
+                                            }
+                                        ) ? ;
+                            $ (
+                                $ (
+                                    url
+                                        .query_pairs_mut()
+                                        .append_pair(
+                                            "text",
+                                            if matches! {
+                                                (
+                                                    self.0 as
+                                                    &$ l $ inner_type
+                                                )
+                                                    .len(),
+                                                $ range
+                                            } { self.0 } else {
+                                                Err($crate
+                                                    ::NekosLifeError
+                                                    ::OutOfRangeError {
+                                                    endpoint_name: stringify! {
+                                                        $ endpoint
+                                                    }.to_owned(),
+                                                    range: $ range,
+                                                })?
+                                            }
+                                        ) ;
+                                ) ?
+                            ) ?
+
+                            url
+                        })
+                    }
+                }
+
+                paste::paste! {
+                    make_text_endpoints! {
+                        $ (
+                            $ (
+                                $ inner
+                            ) ?
+                        ) ?
+                            ||=
+                        [<
+                            $ endpoint :lower
+                        >]
+                            >=>
+                        parse_json |>
+                            [<
+                                $ endpoint
+                                Model
+                            >]
+                            ,
+                    }
+                }
+            }
+        ) +
     };
 }
