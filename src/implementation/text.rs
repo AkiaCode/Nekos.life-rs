aac43a9c0d6dfdd48577bd3e65158fb5e7c2de48
Abiria <abiria.dev@gmail.com>
2022-01-30 13:28:54 +0900

    fix(text): added doc comments

--- src/implementation/text.rs
+++ src/implementation/text.rs
@@ -1,8 +1,15 @@
+#![allow(missing_docs)]
+
 make_text_endpoints! {
+    /// `/cat` endpoint
     Cat =>
+    /// `/owoifu` endpoint
     OwOify @ 'a |> str := owo <| ~~> owoify !# 1..=200 =>
+    /// `/why` endpoint
     Why =>
+    /// `/fact` endpoint
     Fact =>
+    /// `/spoiler` endpoint
     Spoiler @ 'b |> str := owo <| !# 1..=1000
 }
 #[cfg(test)]
